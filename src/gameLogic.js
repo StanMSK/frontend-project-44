6030f26f6c70c3ef1c77a8737ed489a8d46ae403
Stan <147390811+StanMSK@users.noreply.github.com>
2024-03-31 23:34:54 +0500

    Update gameLogic.js

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -22,3 +22,11 @@ export const runGameLogic = (rounds, description) => {
 
   console.log(`Congratulations, ${name}!`);
 };
+
+export function runGame(startRound, description) {
+  const rounds = [];
+  for (let i = 0; i < roundNumber; i += 1) {
+    rounds[i] = startRound();
+  }
+  return runGameLogic(rounds, description);
+}
